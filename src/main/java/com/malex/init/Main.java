e5c7e3dc7405176cdef15479c897ab0d1a0c6589
malexj <alexmaximovj@gmail.com>
2016-06-13 09:23:13 +0300

    fix 1.0.2

--- src/main/java/com/malex/init/Main.java
+++ src/main/java/com/malex/init/Main.java
@@ -5,7 +5,7 @@ import com.malex.service.impl.FactorialServiceImpl;
 
 /**
  * Created by malex on 07.06.16.
- *
+ * <p>
  * Task N3
  * Find sum of the digits in the number 100! (i.e. 100 factorial)
  * {Correct answer: 648}
@@ -14,6 +14,6 @@ public class Main {
     public static void main(String[] args) {
         int number = 100;
         FactorialService factorialService = new FactorialServiceImpl();
-        System.out.println("The sum of the digits in the number 100! -> " + factorialService.getSumFactorialNumbers(number));
+        System.out.println("The sum of the digits in the number " + number + "! -> " + factorialService.getSumFactorialNumbers(number));
     }
 }
